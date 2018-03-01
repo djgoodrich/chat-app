d9ab2a2b06f84b5b53cb75b7d857438827fe5b40
David Goodrich <djgoodrich15@gmail.com>
2018-03-01 11:17:43 -0500

    initial files

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+
+const app = express();
+app.use(express.static(publicPath));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(publicPath + '/index.html');
+// });
+app.listen(3000);
